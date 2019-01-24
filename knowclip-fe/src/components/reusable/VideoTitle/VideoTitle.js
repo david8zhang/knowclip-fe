622bf1a3d7ec432966fee0254be938f19d74e972
David Zhang <david8zhang@berkeley.edu>
2019-01-23 21:00:33 -0800

    fixed clip title too long bug, fixed view truncation

--- knowclip-fe/src/components/reusable/VideoTitle/VideoTitle.js
+++ knowclip-fe/src/components/reusable/VideoTitle/VideoTitle.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './video-title.css';
 
-export const VideoTitle = ({ title, subtitle, views }) => (
-  <div className='videoTitleWrapper'>
-    <div className='videoInfo'>
-      <p className='videoTitle'>{title}</p>
-      <p className='videoSubtitle'>{subtitle}</p>
+export const VideoTitle = ({ title, subtitle, views }) => {
+  let numViews = parseInt(views, 10);
+  let truncViews = views;
+  if (numViews > 1000) {
+    truncViews = `${parseFloat((numViews / 1000).toFixed(1))}K`
+  }
+  if (numViews > 1000000) {
+    truncViews = `${parseFloat((numViews / 1000000).toFixed(1))}M`
+  }
+  return (
+    <div className='videoTitleWrapper'>
+      <div className='videoInfo'>
+        <p className='videoTitle'>{title}</p>
+        <p className='videoSubtitle'>{subtitle}</p>
+      </div>
+      <div className='videoViews'>
+        <p className='views'>{truncViews} Views</p>
+      </div>
     </div>
-    <div className='videoViews'>
-      <p className='views'>{views} Views</p>
-    </div>
-  </div>
-)
+  )
+}
